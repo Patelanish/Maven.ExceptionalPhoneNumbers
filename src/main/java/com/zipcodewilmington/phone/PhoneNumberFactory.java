6f609e4ed72e3a2f76952f348ba7c676f772c363
Anish Patel <anishpatel@MacBook-Pro-9.localdomain>
2019-06-11 08:56:58 -0400

    Finished

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -1,7 +1,9 @@
 package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
+import com.zipcodewilmington.tools.RandomNumberFactory;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -20,15 +22,29 @@ public final class PhoneNumberFactory {
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
     public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+
+        PhoneNumber[] phoneNumbers = new PhoneNumber[phoneNumberCount];
+        for (int i = 0; i < phoneNumberCount; i++) {
+            phoneNumbers[i] = createRandomPhoneNumber();
+        }
+
+        return phoneNumbers;
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
     public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+
+
+
+        return createPhoneNumberSafely(RandomNumberFactory.getRandomAreaCode(), RandomNumberFactory.getCentralOfficeCode(), RandomNumberFactory.getPhoneLineCode());
+
+
+        //  return createPhoneNumberSafely(RandomNumberFactory.getRandomAreaCode(), RandomNumberFactory.getCentralOfficeCode(), RandomNumberFactory.getPhoneLineCode());
     }
+
+
 
 
     /**
@@ -38,15 +54,30 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
     public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
-    }
 
+        String phoneNumberString = "(" + areaCode + ")-" + centralOfficeCode + "-" + phoneLineCode;
+
+        try {
+            return createPhoneNumber(phoneNumberString);
+        } catch (InvalidPhoneNumberFormatException potato) {
+            logger.log(Level.INFO, "Attempting to create a new PhoneNumber object with a  value of " + phoneNumberString , potato);
+            return null;
+
+        }
+
+    }
     /**
      * @param phoneNumberString - some String corresponding to a phone number whose format is `(###)-###-####`
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException {
+
+
+        if (!phoneNumberString.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) {
+            throw new InvalidPhoneNumberFormatException();
+
+        }
+        return new PhoneNumber(phoneNumberString);
     }
 }
--- src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
+++ src/main/java/com/zipcodewilmington/tools/RandomNumberFactory.java
@@ -9,6 +9,7 @@ import java.util.*;
 public abstract class RandomNumberFactory {
     private static final Random random = new Random();
 
+
     /** @return a random float between the specified min and max numeric range */
     public static Float createFloat(float min, float max) {
         return random.nextFloat() * (max - min) + min;
@@ -17,5 +18,19 @@ public abstract class RandomNumberFactory {
     /** @return a random integer between the specified min and max numeric range */
     public static Integer createInteger(Integer min, Integer max) {
         return createFloat(min, max).intValue();
+    }
+
+    public static int getPhoneLineCode() {
+
+        return createInteger(1000, 9999);
+
+    }
+
+    public static int getCentralOfficeCode() {
+        return createInteger(100 , 999);
+    }
+
+    public static int getRandomAreaCode() {
+        return createInteger(100, 999);
     }
 }
